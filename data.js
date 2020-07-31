5b908d7e640500fafc9aa215bada367b2b2e6a89
王水胜 <602466011@qq.com>
2020-07-31 09:42:25 +0800

    更新: data.js

--- data.js
+++ data.js
@@ -14,31 +14,40 @@ const carouselList = [
   }
 ]
 
-const tagsList = [{
-  id: 1,
-  name: '小地家族'
-}, {
-  id: 2,
-  name: '小地单挑'
-}, {
-  id: 3,
-  name: '二十四鲜'
-}, {
-  id: 3,
-  name: '抢鲜'
-}, {
-  id: 4,
-  name: '88折会员卡'
-}, {
-  id: 5,
-  name: '超级年卡'
-}, {
-  id: 6,
-  name: '精选农场'
-}, {
-  id: 7,
-  name: '品牌故事'
-}]
+const tagsList = [
+  {
+    id: 1,
+    name: '小地家族'
+  },
+  {
+    id: 2,
+    name: '小地单挑'
+  },
+  {
+    id: 3,
+    name: '二十四鲜'
+  },
+  {
+    id: 3,
+    name: '抢鲜'
+  },
+  {
+    id: 4,
+    name: '88折会员卡'
+  },
+  {
+    id: 5,
+    name: '超级年卡'
+  },
+  {
+    id: 6,
+    name: '精选农场'
+  },
+  {
+    id: 7,
+    name: '品牌故事'
+  }
+]
 
 const msgs = [
   '果小地以城市、企业、个人为圈层，为会员提供各种折扣，随时关注群动态',
@@ -46,9 +55,48 @@ const msgs = [
   '免费开通88折会员卡，并惠及朋友，请点咨询按钮'
 ]
 
+const partnerList = [
+  {
+    id: 1,
+    name: '酉花 酉果',
+    url: '#',
+    pic: 'https://m.myxiaodi.com/yrg/upload/files/20191224230859jngxI9va.jpg'
+  },
+  {
+    id: 2,
+    name: '优美科技',
+    url: '#',
+    pic: 'https://m.myxiaodi.com/yrg/upload/files/2019122423005753EFU341.jpg'
+  },
+  {
+    id: 3,
+    name: '德蒽 视界',
+    url: '#',
+    pic: 'https://m.myxiaodi.com/yrg/upload/files/20191224225657n8DReQOF.jpg'
+  },
+  {
+    id: 4,
+    name: '自然农法',
+    url: '#',
+    pic: 'https://m.myxiaodi.com/yrg/upload/files/20191224224609Q2kWWR3v.jpg'
+  },
+  {
+    id: 5,
+    name: '微妙军团',
+    url: '#',
+    pic: 'https://m.myxiaodi.com/yrg/upload/files/20191224224055t9ryHaTc.jpg'
+  },
+  {
+    id: 6,
+    name: '会小宝',
+    url: '#',
+    pic: 'https://m.myxiaodi.com/yrg/upload/files/20191224223359BrRGJR6K.jpg'
+  }
+]
 
 export default {
   carouselList,
   tagsList,
-  msgs
+  msgs,
+  partnerList
 }
