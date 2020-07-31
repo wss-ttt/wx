12b1b502ffa528b0049b591481e0a0d9f257ba9c
王水胜 <602466011@qq.com>
2020-07-31 11:51:11 +0800

    更新: main.js 注入store

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import App from './App'
+import store from './store/index'
 
 Vue.config.productionTip = false
 
 App.mpType = 'app'
 
 const app = new Vue({
-    ...App
+    ...App,
+    store // 注入store
 })
 app.$mount()
 Vue.prototype.$BASE_URL = 'https://m.myxiaodi.com/yrg/'
